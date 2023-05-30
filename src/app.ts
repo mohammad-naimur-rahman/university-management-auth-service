f33c56803a67a53c2a612ef3afcb973325c6e345
Naimur <naeemhasan28@gmail.com>
2023-05-30 15:47:54 +0600

    added husky

--- src/app.ts
+++ src/app.ts
@@ -14,6 +14,8 @@ app.use(express.urlencoded({ extended: true }))
 app.use('/api/v1/users/', usersRouter)
 
 //Testing
-app.get('/', async (req: Request, res: Response) => res.send('Working Successfully'))
+app.get('/', async (req: Request, res: Response) =>
+  res.send('Working Successfully')
+)
 
 export default app
