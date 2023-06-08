0a64b0dac61089e257aae19263fbc53f0edf5c22
Naimur <naeemhasan28@gmail.com>
2023-06-08 21:18:05 +0600

    add: response for not found route

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import cors from 'cors'
-import express, { Application, Request, Response } from 'express'
+import express, { Application, NextFunction, Request, Response } from 'express'
+import httpStatus from 'http-status'
 import globalErrorHandler from './middlewares/globalErrorHandler'
 import routes from './routes'
 
@@ -12,10 +13,18 @@ app.use(express.urlencoded({ extended: true }))
 
 app.use('/api/v1/', routes)
 
-app.get('/', async (req: Request, res: Response) =>
-  res.send('Working Successfully')
-)
+app.get('/', async (req: Request, res: Response) => res.send('Hola!'))
 
 app.use(globalErrorHandler)
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.status(httpStatus.NOT_FOUND).json({
+    success: false,
+    message: 'API Not found!',
+    errorMessages: [{ path: req.originalUrl, message: 'API Not found!' }],
+    stack: '',
+  })
+  next()
+})
 
 export default app
