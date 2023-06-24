8d62028e9938f4d7e2af17768a4b520cc5715537
Naimur <naeemhasan28@gmail.com>
2023-06-24 16:56:20 +0600

    beautify: user model and interface

--- src/app/modules/user/user.interface.ts
+++ src/app/modules/user/user.interface.ts
@@ -14,14 +14,6 @@ export type IUser = {
   admin?: Types.ObjectId | IAdmin
 }
 
-// export type IUserMethods = {
-//   isUserExist(id: string): Promise<Partial<IUser> | null>;
-//   isPasswordMatched(
-//     givenPassword: string,
-//     savedPassword: string
-//   ): Promise<boolean>;
-// };
-
 export type UserModel = {
   isUserExist(
     id: string
@@ -31,5 +23,3 @@ export type UserModel = {
     savedPassword: string
   ): Promise<boolean>
 } & Model<IUser>
-
-// export type UserModel = Model<IUser, Record<string, unknown>, IUserMethods>;
